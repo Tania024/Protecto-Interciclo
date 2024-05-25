fafc19d6aad80a9077d96d2925d4eb5512c01070
Tania024 <84852408+Tania024@users.noreply.github.com>
2024-05-25 15:30:40 -0500

    instalacion del firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"proyectointerciclo-9b608","appId":"1:496817323580:web:70a600906732f3888a8d39","storageBucket":"proyectointerciclo-9b608.appspot.com","apiKey":"AIzaSyDGNMQyP9CmnlXa6RNbKi3_38r3_7vtE0w","authDomain":"proyectointerciclo-9b608.firebaseapp.com","messagingSenderId":"496817323580"})), provideFirestore(() => getFirestore())]
 };
