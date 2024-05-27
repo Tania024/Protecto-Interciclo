67d032f0d26eee71358e15903f351319f125a2a4
ErikaContreras15 <econtrerasz@est.ups.edu.ec>
2024-05-27 06:11:41 -0500

    Iniciar sesion/registrarse/Pag Carrito
    Diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent],
+  imports: [RouterOutlet, InicioComponent,CarritoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
